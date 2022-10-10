db73f3b4015d87aad3e66f18eb9a2551efe0b396
Chris Tomshack <ctomshack15@gmail.com>
2022-10-10 19:27:55 -0400

    removed and re-added node modules & package-lock.json

--- next.config.js
+++ next.config.js
@@ -4,11 +4,10 @@ const nextConfig = {
   swcMinify: true,
 }
 
-// module.exports = nextConfig
+module.exports = nextConfig
 
 module.exports = {
   images: {
     domains: ['images.unsplash.com']
-  },
-  nextConfig,
+  }
 }
