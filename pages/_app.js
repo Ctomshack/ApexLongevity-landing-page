dbedda5326dad9a8ae017fdc410f6e7c196aaf22
Chris Tomshack <ctomshack15@gmail.com>
2022-10-17 17:35:37 -0400

    added SEO tags and list of keywords in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,46 @@
-import Script from 'next/script'
-import '../styles/globals.css'
+import Script from "next/script";
+import SEO from "@bradgarropy/next-seo";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-  <>
-  <Script
-    strategy="lazyOnload"
-    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-  />
+    <>
+      <SEO
+        title="Apex Longevity"
+        description="Compliant cash discounts for chiropractors and a discount medical program for chirorpactic patients nationwide"
+        keywords={[
+          "chiropractor",
+          "chiropractic",
+          "dmpo",
+          "discount medical plan",
+          "discount medical plan organization",
+          "apex",
+          "apex longevity",
+          "CHUSA",
+          "chirohealthusa",
+          "chirohealth",
+          "chiro health",
+          "chiro health usa",
+          "chiropractor cash discount",
+          "uninsured",
+        ]}
+      />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
 
-  <Script id="google-analytics" strategy="lazyOnload">
-    {`window.dataLayer = window.dataLayer || [];
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
       gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
-  </Script>
+      </Script>
 
-  <Component {...pageProps} />
-</>
-)
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
